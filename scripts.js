c36b418781ff0023c8e6b620dc7f96831c17a829
zkec <hoomantitan@gmail.com>
2024-01-13 19:16:34 +0330

    commit all files to the repository

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//animation class after the page has loaded :)
+document.addEventListener("DOMContentLoaded", function () {
+  const loginBox = document.querySelector(".login-box");
+  loginBox.classList.add("animated");
+});
